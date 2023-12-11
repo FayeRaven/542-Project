eae70a9888bf4d5eb1fab17a6c1912c00658f058
Christopher Ferreira <99435042+FayeRaven@users.noreply.github.com>
2023-12-11 15:15:09 -0500

    Update main.py
    Removed lines that belong in README

--- main.py
+++ main.py
@@ -2,21 +2,6 @@
 # Christopher Ferreira
 # CIS 542 Project
 #
-# 542-Project
-# This code is designed to take in 2 directories and compare each file in the first
-# directory to each file in the second directory. It will print the number of
-# files, along with the files themselves, that share the same hash values but
-# have different file names. It will also print the number of files and the files
-# that share a file name but have different hash values.
-#
-# This program is not designed to interpret the data printed!
-# It simply returns raw data and requires the user to determine what that data means.
-#
-# This program uses two particular python libraries: hashlib and os
-# Hashlib is used to retrieve the hash values of files and os
-# is usd to retrieve all the files ans filepaths used.
-
-
 
 import hashlib
 import os
